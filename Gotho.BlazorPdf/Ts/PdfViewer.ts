9c0d1bb86cd009b1208cc614a36479a8486a0896
Hugo Peters <mail@hugo.fyi>
2026-04-23 21:36:05 +0200

    do not send credentials

--- Gotho.BlazorPdf/Ts/PdfViewer.ts
+++ Gotho.BlazorPdf/Ts/PdfViewer.ts
@@ -352,7 +352,7 @@ function getDocumentInit(pdfDto: PdfState) {
     if (pdfDto.password)
         documentInit.password = pdfDto.password;
 
-    documentInit.withCredentials = true;
+    documentInit.withCredentials = false;
 
     return documentInit;
 }
